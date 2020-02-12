3088a60310260bf237b62a2c0791b7fe419b7c7c
abida hassan <jeanarnolde1995@gmail.com>
2020-02-12 16:04:33 +0100

    second commit

--- projet-boot-taxBoisson/src/main/java/com/example/taxBoisson/bean/Categorie.java
+++ projet-boot-taxBoisson/src/main/java/com/example/taxBoisson/bean/Categorie.java
@@ -1,17 +1,19 @@
 package com.example.taxBoisson.bean;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Categorie  implements Serializable {	
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
